dde484e0341d4b00efec035b72da6406afde792f
Jared Salzano <jaredsalzano@gmail.com>
2025-01-10 17:12:52 -0500

    Fix marquee horizontal scroll on mobile devices

--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -3,7 +3,7 @@ import '../styles/marquee.scss';
 
 export default function Marquee({ children, className = '' }: { children: ReactNode, className: string }) {
     return (
-        <div className={`marquee flex gap-4 ${className}`}>
+        <div className={`marquee overflow-hidden flex gap-4 ${className}`}>
             <div className='flex-shrink-0 flex gap-4 marquee-group'>{children}</div>
             <div aria-hidden className='flex-shrink-0 flex gap-4 marquee-group'>{children}</div>
         </div>
