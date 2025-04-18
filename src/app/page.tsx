f385bbfd4bff4b12daf1cad8516924c0508b7948
Jared Salzano <jaredsalzano@gmail.com>
2025-04-18 12:21:42 -0400

    Remove extra div on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,31 +28,27 @@ export default async function Home() {
     <>
       <a className="absolute right-2 underline z-10 text-blue-700" href="/other-stuff">other stuff</a>
       <main className="relative">
-        <div className="bg-orange-50" style={{ backgroundColor: "rgba(255, 255, 255, .9)" }}>
-          <section className="page-container pt-28" aria-label="Intro">
-            <div className="flex flex-col md:flex-row items-start gap-8">
-              <Image
-                src="/linkedin.jpg"
-                alt="Jared Salzano portrait"
-                width={150}
-                height={150}
-                className="rounded-lg"
-              />
-              <div className="prose prose-xl mb-16">
-                <p><b className="font-[500]">Hi, I&apos;m a front-end-leaning full-stack developer</b> with 8+ years of experience shipping maintainable, high-performance web apps for fast-moving startups, creative agencies, and Fortune 50 companies. I take ownership of projects from planning to deployment, solve complex technical challenges, and collaborate effectively across teams. Lately, I&apos;ve been exploring LLMs and emerging AI cloud services.</p>
-                <p>I&apos;m <b className="font-[500]">actively looking for a full-time role</b> at a thoughtful, purpose-driven startup—NYC or remote—as a senior front-end or full-stack developer. I&apos;m also <b className="font-[500]">currently taking on select freelance projects</b>, always excited to work with new people on fresh challenges.</p>
-                <p className="mb-10">You can see my full resume <a target="_blank" href="/resume.pdf">here</a>.</p>
-                <a className="btn rounded-2xl" target="_blank" href="mailto:jaredsalzano@gmail.com">💬 Message me</a>
-              </div>
-            </div>
-          </section>
-          <section aria-label="Projects">
-            <ProjectGrid posts={posts} />
-          </section>
-          <section className="bg-orange-50 py-4" aria-label="Testimonials">
-            <Testimonials />
-          </section>
-        </div>
+        <section className="page-container pt-28 flex flex-col md:flex-row items-start gap-8" aria-label="Intro" style={{ backgroundColor: "rgba(255, 255, 255, .9)" }}>
+          <Image
+            src="/linkedin.jpg"
+            alt="Jared Salzano portrait"
+            width={150}
+            height={150}
+            className="rounded-lg"
+          />
+          <div className="prose prose-xl mb-16">
+            <p><b className="font-[500]">Hi, I&apos;m a front-end-leaning full-stack developer</b> with 8+ years of experience shipping maintainable, high-performance web apps for fast-moving startups, creative agencies, and Fortune 50 companies. I take ownership of projects from planning to deployment, solve complex technical challenges, and collaborate effectively across teams. Lately, I&apos;ve been exploring LLMs and emerging AI cloud services.</p>
+            <p>I&apos;m <b className="font-[500]">actively looking for a full-time role</b> at a thoughtful, purpose-driven startup—NYC or remote—as a senior front-end or full-stack developer. I&apos;m also <b className="font-[500]">currently taking on select freelance projects</b>, always excited to work with new people on fresh challenges.</p>
+            <p className="mb-10">You can see my full resume <a target="_blank" href="/resume.pdf">here</a>.</p>
+            <a className="btn rounded-2xl" target="_blank" href="mailto:jaredsalzano@gmail.com">💬 Message me</a>
+          </div>
+        </section>
+        <section aria-label="Projects">
+          <ProjectGrid posts={posts} />
+        </section>
+        <section className="bg-orange-50 py-4" aria-label="Testimonials">
+          <Testimonials />
+        </section>
         <section style={{ backgroundColor: "rgba(255, 255, 255, .9)" }} aria-label="Frequently asked questions">
           <FAQ />
         </section>
