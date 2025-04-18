775fda1836cd6b7a6551b2f0afa73b9dc1b65e33
Jared Salzano <jaredsalzano@gmail.com>
2025-04-18 12:32:27 -0400

    Remove page-container class

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,7 +46,7 @@ export default async function Home() {
         <section aria-label="Projects">
           <ProjectGrid posts={posts} />
         </section>
-        <section className="page-container bg-orange-50 py-4" aria-label="Testimonials">
+        <section className="bg-orange-50 py-4" aria-label="Testimonials">
           <Testimonials />
         </section>
         <section className="page-container py-16" style={{ backgroundColor: "rgba(255, 255, 255, .9)" }} aria-label="Frequently asked questions">
